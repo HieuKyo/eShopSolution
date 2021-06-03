8f36344e67de7c5fb4ae5374462ac1f0d000890d
Your Name <hieutt291199@gmail.com>
2021-06-03 15:16:45 +0700

    Add project files.

---
+++ eShopSolution.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using eShopSolution.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+//Chứa toàn bộ Controller hệ thống, xủ7 lý request
+namespace eShopSolution.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+//Thư mục Model chứ các model dữ liệu
+namespace eShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
